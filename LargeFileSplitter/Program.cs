983f0a46b324b4cad439e98b8f68dcef217a43e5
David <David@c0degeek.dev>
2021-10-23 16:42:46 +0200

    added

---
+++ LargeFileSplitter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+var fileCount = 1;
+const int maximumLinesPerFile = 15000000 * 5;
+
+StreamWriter? writer = null;
+try
+{
+    using StreamReader inputFile = new StreamReader(@"C:\CompiledWordList.txt");
+    var count = 0;
+    string? line;
+    while ((line = inputFile.ReadLine()) != null)
+    {
+
+        if (writer == null || count > maximumLinesPerFile)
+        {
+            if (writer != null)
+            {
+                writer.Close();
+                writer = null;
+                fileCount++;
+            }
+
+            writer = new StreamWriter(@$"D:\hashcat\hashcat-3.6.0\CompiledWordList{fileCount}.txt", true);
+
+            count = 0;
+        }
+
+        writer.WriteLine(line.ToLower());
+
+        ++count;
+    }
+}
+finally
+{
+    writer?.Close();
+}
---
+++ LargeTextParser/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+const int cutoff = 50308538;
+
+using var sr = File.OpenText(@"C:\all_in_one_w\all_in_one_w");
+
+var stringBuilder = new StringBuilder();
+string? line;
+var count = 0;
+long totalCount = 0;
+
+while((line = sr.ReadLine()) != null){
+    if (line.Length is < 8 or > 16)
+        continue;
+    
+    if(HasConsecutiveChars(line, 3))
+        continue;
+
+    stringBuilder.AppendLine(line);
+    count++;
+    if (count == cutoff)
+    {
+        Console.WriteLine("Cutoff reached, writing file");
+        totalCount += count;
+        count = 0;
+        File.AppendAllText(@"D:\CompiledWordList.txt", stringBuilder.ToString());
+        stringBuilder.Clear();
+        stringBuilder = new StringBuilder();
+    }
+}
+
+Console.WriteLine($"A total of {totalCount} possible passwords written");
+
+
+static bool HasConsecutiveChars(string source, int sequenceLength)
+{
+    var charEnumerator = StringInfo.GetTextElementEnumerator(source);
+    var currentElement = string.Empty;
+    var count = 1;
+    while (charEnumerator.MoveNext()) 
+    {
+        if (currentElement == charEnumerator.GetTextElement())
+        {
+            if (++count >= sequenceLength)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            count = 1;
+            currentElement = charEnumerator.GetTextElement();
+        }
+    }
+    return false;
+}
